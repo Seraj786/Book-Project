a77622a6ea814853e9c634db8f86de6fcf96ef4a
Seraj786 <raj.siwani2009@gmail.com>
2025-07-23 17:20:37 +0530

    Dev - remove json server

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,35 +1,38 @@
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
-import Header from './Header'
-import { useEffect, useState } from 'react';
-import { ProductContext } from '../context/ProductContext';
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import Header from "./Header";
+import { useEffect, useState } from "react";
+import { ProductContext } from "../context/ProductContext";
+import { API_BASE_URL } from "../constant/apiConstant";
 
 const Layout = () => {
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  const fetchData = async () => {
+    const response = await fetch(`${API_BASE_URL}/products`);
+    console.log("===API_BASE_URL", API_BASE_URL);
+    const result = await response.json();
+    setData(result);
+  };
 
-    const fetchData = async () => {
-        const response = await fetch('http://localhost:3000/products');
-        const result = await response.json();
-        setData(result)
-    };
-
-    return (
-        <div>
-            <ProductContext.Provider value={{ data, setData }}>
-                {data.length === 0 ? <div>Loading...</div> :
-                    <>
-                        <Header />
-                        <Outlet />
-                    </>
-                }
-            </ProductContext.Provider>
-            <Footer />
-        </div>
-    )
-}
-export default Layout
+  return (
+    <div>
+      <ProductContext.Provider value={{ data, setData }}>
+        {data.length === 0 ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            <Header />
+            <Outlet />
+          </>
+        )}
+      </ProductContext.Provider>
+      <Footer />
+    </div>
+  );
+};
+export default Layout;
